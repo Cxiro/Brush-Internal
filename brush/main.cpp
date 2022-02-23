d26917beec6073c714adf5fcebd0be8c46221385
Expando <70826967+Cxiro@users.noreply.github.com>
2022-02-22 19:57:16 -0500

    setup

--- brush/main.cpp
+++ brush/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <Windows.h>
+#include <thread>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+ void brush(){
+
+ }
+
+int _stdcall DllMain(void*, int r, void*) {
+    if(r==1)
+        std::thread(brush).detach();
     return 0;
 }
